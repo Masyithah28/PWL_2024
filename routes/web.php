77178c03efae5b09039ec3c59e0d11fd41a7543b
Masyithah28 <masyithahsophia@gmail.com>
2024-02-23 19:59:18 +0700

    3 modifikasi

--- routes/web.php
+++ routes/web.php
@@ -60,3 +60,13 @@ Route::resource('photos', PhotoController::class)->only([ 'index', 'show'
 
 Route::resource('photos', PhotoController::class)->except([ 'create', 'store', 'update', 'destroy'
 ]);
+
+Route::get('/greeting', function () {
+   return view('hello', ['name' => 'Masyithah']);
+   });
+   
+   Route::get('/greeting', function () {
+      return view('blog.hello', ['name' => 'Masyithah']);
+      });
+     
+      Route::get('/greeting', [WelcomeController::class, 'greeting']);
